b460b62dc28150b5820402512f82a85f5bd7dfb7
ArielValdez <arielvaldez017@outlook.com>
2022-05-10 07:01:05 -0400

    Documentacion interna agregada

--- Proyecto Tendencias/TD.01/Program.cs
+++ Proyecto Tendencias/TD.01/Program.cs
@@ -6,6 +6,7 @@ namespace TD._01
     {
         static void Main(string[] args)
         {
+            #region manual tests
             //SSNArray(" ");
             //SSNArray("000-00-0000");
             //SSNArray("333-01-0001");
@@ -17,9 +18,11 @@ namespace TD._01
             //SSNArray("333-99-0000");
             //SSNArray("333-99-100");
             //SSNArray("333-99-3333");
+            #endregion
             try
             {
-                Console.WriteLine(SSNRegex(args[0]));
+                //Prints the message Valid or Invalid
+                //Console.WriteLine(SSNRegex(args[0]));
                 Console.WriteLine("Introduced SSN is: {0}", SSNArray(args[0]));
             }
             catch (Exception ex)
@@ -29,15 +32,23 @@ namespace TD._01
         }
         static string SSNArray(string ssn)
         {
+            //These arrays are used to separate the sections of the SSN into different indexes
             char[] separator = { '-', ' ' };
             string[] n = ssn.Split(separator);
+            //This loop checks if the SSN is not empty, or if it has more than three sections
             for (int i = 0; i < n.Length; i++)
             {
+                if (i > 2)
+                {
+                    return "Invalid";
+                }
                 if (n[i] == null)
                 {
                     return "Invalid";
                 }
             }
+            //This regions validates an SSN, given the requirements
+            #region SSN Validation
             if (n[0].Length != 3 || int.Parse(n[0]) == 666 || int.Parse(n[0]) == 0 || (int.Parse(n[0]) > 899 && int.Parse(n[0]) < 1000))
             {
                 return "Invalid";
@@ -51,9 +62,11 @@ namespace TD._01
                 return "Invalid";
             }
             else return "Valid";
+            #endregion
         }
-        static string SSNRegex(string ssn)
+        static string SSNRegex(string ssn) //Alternative
         {
+            //Regular expression, used to check to match the SSN and the regex used
             Regex regex = new Regex("^(?!666|000|9\\d{2})\\d{3}-(?!00)\\d{2}-(?!0{4})\\d{4}$");
             //Regex regex = new Regex(@"^\d{3}\-?\d{2}\-?\d{4}$");
             if (ssn == null)
@@ -62,6 +75,7 @@ namespace TD._01
             }
             else
             {
+                //Checks for a matching result
                 bool matching = regex.IsMatch(ssn);
 
                 if (matching)
